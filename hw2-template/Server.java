b1de32674d579c9c68ed9e12c53dafe4129cfb19
Vijay Garg <vkgarg@gmail.com>
2019-09-13 12:39:06 -0500

    hw2 added

---
+++ hw2-template/Server.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Server {
+  public static void main (String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int myID = sc.nextInt();
+    int numServer = sc.nextInt();
+    int numSeat = sc.nextInt();
+
+
+    for (int i = 0; i < numServer; i++) {
+      // TODO: parse inputs to get the ips and ports of servers
+    }
+
+    // TODO: handle request from clients
+  }
+}
